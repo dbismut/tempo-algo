d03f58e57fb1626bc7118541fbd86b91319b39c2
David Bismut <david.bismut@gmail.com>
2022-10-25 11:06:38 +0200

    crossed polygons

--- src/algos/area.ts
+++ src/algos/area.ts
@@ -15,9 +15,43 @@ export const area = (s1: SongData, s2: SongData, visualize: string) => {
 
 	const l = points.length
 	const areas: number[] = []
-	for (let i = 0; i < l / 2; i++) {
-		const poly = points.slice(i, i + 2).concat(points.slice(l - i - 2, l - i))
-		areas.push(Math.abs(d3.polygonArea(poly)))
+	for (let i = 1; i < l / 2; i++) {
+		const [p1, p2] = points.slice(i, i + 2) as [Point, Point]
+		const [p3, p4] = points.slice(l - i - 2, l - i) as [Point, Point]
+		const intersec = getIntersection([p1, p2], [p3, p4])
+		const [ix, iy] = intersec
+		const dxi = ix - p1[0]
+		const dyi = iy - p1[1]
+		const dxl = p2[0] - p1[0]
+		const dyl = p2[1] - p1[1]
+
+		const isPolygonCrossed =
+			Math.sign(dxi) === Math.sign(dxl) &&
+			Math.sign(dyi) === Math.sign(dyl) &&
+			Math.abs(dxi) <= Math.abs(dxl) &&
+			Math.abs(dyi) <= Math.abs(dyl)
+
+		const area = isPolygonCrossed
+			? Math.abs(d3.polygonArea([p1, intersec, p4])) + Math.abs(d3.polygonArea([p2, intersec, p3]))
+			: Math.abs(d3.polygonArea([p1, p2, p3, p4]))
+
+		areas.push(area)
 	}
 	return sum(areas)
+}
+
+type Point = [number, number]
+
+function getLineEq([x1, y1]: Point, [x2, y2]: Point) {
+	const a = (y2 - y1) / (x2 - x1)
+	const b = y1 - a * x1
+	return [a, b]
+}
+
+function getIntersection(l1: [Point, Point], l2: [Point, Point]): Point {
+	const [a1, b1] = getLineEq(...l1)
+	const [a2, b2] = getLineEq(...l2)
+	const x = (b2 - b1) / (a1 - a2)
+	const y = a1 * x + b1
+	return [x, y]
 }
